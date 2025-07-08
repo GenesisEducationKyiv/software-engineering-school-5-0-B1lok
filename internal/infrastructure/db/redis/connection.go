e671952d336b988cce807649ec59fea4a93a5a0b
Bilokur Evgen <bilokur915@gmail.com>
2025-07-08 10:44:11 +0300

    extracted constant variables & added dialTimeout

--- internal/infrastructure/db/redis/connection.go
+++ internal/infrastructure/db/redis/connection.go
@@ -9,13 +9,20 @@ import (
 	"weather-api/internal/config"
 )
 
+const (
+	readTimeout  = 2 * time.Second
+	writeTimeout = 2 * time.Second
+	dialTimeout  = 3 * time.Second
+)
+
 func NewClient(ctx context.Context, cfg config.RedisConfig) (*redis.Client, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr:         cfg.Address,
 		Password:     cfg.Password,
 		DB:           cfg.DB,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		DialTimeout:  dialTimeout,
 	})
 
 	err := client.Ping(ctx).Err()
