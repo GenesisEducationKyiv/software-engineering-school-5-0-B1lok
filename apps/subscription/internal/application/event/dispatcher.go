b71f95d7b5d219bcdb50b911fd030988175d6ad0
Bilokur Evgen <bilokur915@gmail.com>
2025-07-19 21:04:52 +0300

    added application events

---
+++ apps/subscription/internal/application/event/dispatcher.go
@@ -0,0 +1,29 @@
+package event
+
+import (
+	"context"
+	"fmt"
+)
+
+type Dispatcher struct {
+	handlers []Handler
+}
+
+func NewDispatcher() *Dispatcher {
+	return &Dispatcher{
+		handlers: make([]Handler, 0),
+	}
+}
+
+func (d *Dispatcher) Register(handler Handler) {
+	d.handlers = append(d.handlers, handler)
+}
+
+func (d *Dispatcher) Dispatch(ctx context.Context, event Event) error {
+	for _, handler := range d.handlers {
+		if handler.CanHandle(event.EventName()) {
+			return handler.Handle(ctx, event)
+		}
+	}
+	return fmt.Errorf("no handler found for event: %s", event.EventName())
+}
---
+++ apps/subscription/internal/application/event/handler.go
@@ -0,0 +1,8 @@
+package event
+
+import "context"
+
+type Handler interface {
+	Handle(ctx context.Context, event Event) error
+	CanHandle(name Name) bool
+}
